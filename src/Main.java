8c2a25f98d9a77c9c122fcdce6591109ef8a33bf
Obyduzzaman-Abir <obyduzzamanabir@gmail.com>
2024-12-04 22:01:48 +0600

    object and Class

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        Teacher teacher1=new Teacher();
+        teacher1.name="Anisul Sir";
+        teacher1.gender="Male";
+        teacher1.phone=01452;
+
+        Teacher teacher2=new Teacher();
+        teacher2.name="Me";
+        teacher2.gender="Male";
+        teacher2.phone=025545;
+
+        System.out.println("Name : "+teacher1.name+"gender : " +teacher1.gender+"Phone : "+teacher1.phone);
+        System.out.println("Name : "+teacher2.name+"gender : "+teacher2.gender+"Phone : "+teacher2.phone);
+
+
+    }
+
+
+
+}
