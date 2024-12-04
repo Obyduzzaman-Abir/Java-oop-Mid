a5f032a06328e5b3121184c4f257049bb86556a7
Obyduzzaman-Abir <obyduzzamanabir@gmail.com>
2024-12-04 22:16:14 +0600

    This is method without perameter in class

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,17 @@ public class Main {
         teacher1.name="Anisul Sir";
         teacher1.gender="Male";
         teacher1.phone=01452;
+        teacher1.displayResult();;
 
         Teacher teacher2=new Teacher();
         teacher2.name="Me";
         teacher2.gender="Male";
         teacher2.phone=025545;
+        teacher2.displayResult();
 
-        System.out.println("Name : "+teacher1.name+"gender : " +teacher1.gender+"Phone : "+teacher1.phone);
+       /* System.out.println("Name : "+teacher1.name+"gender : " +teacher1.gender+"Phone : "+teacher1.phone);
         System.out.println("Name : "+teacher2.name+"gender : "+teacher2.gender+"Phone : "+teacher2.phone);
-
+*/
 
     }
 
--- src/Teacher.java
+++ src/Teacher.java
@@ -1,4 +1,9 @@
 public class Teacher {
     String name,gender;
     int phone;
+    void displayResult()
+    {
+        System.out.println("Name : "+name+"gender : " +gender +"  Phone : "+phone);
+
+    }
 }
