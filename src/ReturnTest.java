ac2dbea38d1d5601f4a84fbacf1421abfacf6364
Obyduzzaman-Abir <obyduzzamanabir@gmail.com>
2024-12-06 01:20:28 +0600

    basic constraction and return value

---
+++ src/ReturnTest.java
@@ -0,0 +1,7 @@
+public class ReturnTest {
+    public static void main(String[] args) {
+        ReturningValue ob1=new ReturningValue();
+        int result=  ob1.square(5);
+        System.out.println("square  " + result);
+    }
+}
---
+++ src/ReturningValue.java
@@ -0,0 +1,6 @@
+public class ReturningValue {
+    int square(int value)
+    {
+        return value*value;
+    }
+}
