f0dec38a43e938b16b877d6aa0383f512202a457
Obyduzzaman-Abir <obyduzzamanabir@gmail.com>
2024-12-06 22:50:28 +0600

    Counter

---
+++ src/StaticStudent3.java
@@ -0,0 +1,13 @@
+public class StaticStudent3 {
+
+     static  int count=0;
+    StaticStudent3 ()
+    {
+        count++;
+    }
+    void TotalStu()
+    {
+        System.out.println("Total Student : " + count);
+    }
+
+}
---
+++ src/StaticStudentTest3.java
@@ -0,0 +1,14 @@
+public class StaticStudentTest3 {
+    public static void main(String[] args) {
+        StaticStudent3 s1=new StaticStudent3();
+        s1.TotalStu();
+        StaticStudent3 s2=new StaticStudent3();
+        s2.TotalStu();
+        StaticStudent3 s3=new StaticStudent3();
+        StaticStudent3 s4=new StaticStudent3();
+
+        s3.TotalStu();
+
+    }
+
+}
