0cf2590f24cc4a0b278c62be1a744b08ba51b65e
Obyduzzaman-Abir <obyduzzamanabir@gmail.com>
2024-12-18 13:53:40 +0600

    Enheritence

---
+++ src/EnCapTest1.java
@@ -0,0 +1,11 @@
+public class EnCapTest1 {
+    public static void main(String[] args) {
+        Encapsulation1 p1=new Encapsulation1();
+        p1.setName("Anis");
+        System.out.println(p1.getName());
+        p1.setAge(23);
+        System.out.println(p1.getAge());
+
+    }
+
+}
